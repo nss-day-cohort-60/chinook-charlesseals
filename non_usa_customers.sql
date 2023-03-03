1ea0e690f3441137be5096e6461b6741b77aa306
Charles Seals <charlesseals11@gmail.com>
2023-03-03 13:32:02 -0600

    added SQL for non_usa_custoers

---
+++ non_usa_customers.sql
@@ -0,0 +1,7 @@
+Provide a query showing Customers who are not in the US. The resultant table should include:
+
+Customer's full name
+Customer Id,
+Customer's country
+
+SELECT * FROM Customer WHERE Country != 'USA'
